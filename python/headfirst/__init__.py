da4c117f295b2c47252181a83e28a462ed88fdee
notqualia <qiguanyu2022@outlook.com>
2020-08-16 13:51:40 +0800

    delete

--- python/headfirst/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-_HEADFIRST_PY = os.path.dirname(os.path.abspath(__file__))
-HEADFIRST_BASE = os.path.dirname(_HEADFIRST_PY)
--- python/headfirst/tests/_support.py
+++
@@ -1,9 +0,0 @@
-import sys
-from io import BytesIO
-
-
-def call_and_capture_output(func):
-    sys.stdout = buf = BytesIO()
-    func()
-    sys.stdout = sys.__stdout__
-    return buf.getvalue()
